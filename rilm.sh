7caf17b292c6475a11178ed74528264129e7f7b7
benkdrager <benkdrag@gmail.com>
2025-11-19 16:02:33 +0100

    Update rilm.sh

--- rilm.sh
+++ rilm.sh
@@ -44,6 +44,7 @@ done
 echo "------------------------------------------------------"
 echo "Synchronisierung abgeschlossen.
 "
+cd $HOME/rilm
 
 ##Abruf und Transformation der RILM-Daten
 ## Abfrage des aktuellen RILM-Stempels "JJJJ-MM-TT"
